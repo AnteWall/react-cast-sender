7b0a2e32dacf83b1ab40717b37c60b47774e71db
Ante Wall <antewall@gmail.com>
2019-05-23 16:59:59 +0200

    Updates

---
+++ precache-manifest.b6aec65a554fbd6de6209bf673156214.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96e6121cafedf9af91a3d7e1f266ec64",
+    "url": "/react-cast-sender/index.html"
+  },
+  {
+    "revision": "5a7d43ad2b42975cad35",
+    "url": "/react-cast-sender/static/css/main.3d7a58b6.chunk.css"
+  },
+  {
+    "revision": "816dc152bff1a8dd9f6a",
+    "url": "/react-cast-sender/static/js/2.0240e014.chunk.js"
+  },
+  {
+    "revision": "5a7d43ad2b42975cad35",
+    "url": "/react-cast-sender/static/js/main.06bb14a8.chunk.js"
+  },
+  {
+    "revision": "3bf53d6bf6f2b308230a",
+    "url": "/react-cast-sender/static/js/runtime~main.d869d019.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c6cf04dfe04bae0f9c7e1105b6782f6b.js"
+  "/react-cast-sender/precache-manifest.b6aec65a554fbd6de6209bf673156214.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-cast-sender/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
