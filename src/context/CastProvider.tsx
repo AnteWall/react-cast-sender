71e5493cd2b0243031d61e5b06225ef130bb3e13
Ante Wall <antewall@gmail.com>
2019-06-10 14:36:53 +0200

    fix: Initialize issue

--- src/context/CastProvider.tsx
+++ src/context/CastProvider.tsx
@@ -64,7 +64,7 @@ const CastProvider = ({
       }
     };
 
-    if (castInitialized && window.cast) {
+    if (!castInitialized && window.cast) {
       window.cast.framework.CastContext.getInstance().setOptions({
         receiverApplicationId,
         resumeSavedSession,
